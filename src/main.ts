14c2340fa0e137253b6ad2d029145d8255a57249
sopheaktra_eang <sopheaktra.eang@wingbank.com.kh>
2024-10-08 14:27:34 +0700

    change swagger doc url.

--- src/main.ts
+++ src/main.ts
@@ -61,9 +61,7 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
     ],
-    swaggerOptions: {
-      url: '/swagger.json',
-    },
+    jsonDocumentUrl: '/swagger.json',
   });
 
   await app.listen(3000);
