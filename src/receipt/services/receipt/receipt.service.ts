193617fbc15bf5cb14f468013518cd077caffb8f
sopheaktra_eang <sopheaktra.eang@wingbank.com.kh>
2024-10-09 14:31:03 +0700

    updated

--- src/receipt/services/receipt/receipt.service.ts
+++ src/receipt/services/receipt/receipt.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@nestjs/common';
 import * as hbs from 'handlebars';
 import * as fs from 'fs';
 import { join } from 'path';
-import * as puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 import chromium from 'chrome-aws-lambda';
-
 @Injectable()
 export class ReceiptService {
   async create(createReceiptDto, baseUrl: string): Promise<Buffer> {
@@ -22,22 +21,32 @@ export class ReceiptService {
     const renderedHtml = template({ ...createReceiptDto, baseUrl });
 
     // Use puppeteer-core with chrome-aws-lambda in production
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: true, // Run headless mode
-      defaultViewport: chromium.defaultViewport,
-    });
+    let browser;
+    try {
+      browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: true, // Run headless mode
+        defaultViewport: chromium.defaultViewport,
+      });
+    } catch (error) {
+      console.error('Error launching browser:', error);
+      throw new Error('Failed to launch browser.');
+    }
 
     const page = await browser.newPage();
     await page.setContent(renderedHtml, { waitUntil: 'networkidle0' });
 
-    const pdfBuffer = await page.pdf({
-      format: 'a6',
+    // Generate the PDF (which returns a Uint8Array)
+    const pdfUint8Array = await page.pdf({
+      format: 'A4',
       printBackground: true,
     });
 
+    // Convert Uint8Array to Buffer
+    const pdfBuffer = Buffer.from(pdfUint8Array);
+
     await browser.close();
-    return Buffer.from(pdfBuffer);
+    return pdfBuffer;
   }
 }
