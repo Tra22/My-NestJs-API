bdd09f69cf30d51726b3fa4813653e73a37afe79
sopheaktra_eang <sopheaktra.eang@wingbank.com.kh>
2024-10-09 14:34:01 +0700

    updated.

--- src/receipt/services/receipt/receipt.service.ts
+++ src/receipt/services/receipt/receipt.service.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 import { join } from 'path';
 import puppeteer from 'puppeteer-core';
 import chromium from 'chrome-aws-lambda';
+
 @Injectable()
 export class ReceiptService {
   async create(createReceiptDto, baseUrl: string): Promise<Buffer> {
@@ -20,15 +21,19 @@ export class ReceiptService {
     const template = hbs.compile(templateFile);
     const renderedHtml = template({ ...createReceiptDto, baseUrl });
 
-    // Use puppeteer-core with chrome-aws-lambda in production
     let browser;
     try {
+      // Logging to check executable path
+      console.log('Using executable path:', await chromium.executablePath);
+
       browser = await puppeteer.launch({
         args: chromium.args,
-        executablePath: await chromium.executablePath,
+        executablePath: await chromium.executablePath, // Use the executable path from chromium-aws-lambda
         headless: true, // Run headless mode
         defaultViewport: chromium.defaultViewport,
       });
+
+      console.log('Browser launched successfully');
     } catch (error) {
       console.error('Error launching browser:', error);
       throw new Error('Failed to launch browser.');
@@ -37,13 +42,11 @@ export class ReceiptService {
     const page = await browser.newPage();
     await page.setContent(renderedHtml, { waitUntil: 'networkidle0' });
 
-    // Generate the PDF (which returns a Uint8Array)
     const pdfUint8Array = await page.pdf({
       format: 'A4',
       printBackground: true,
     });
 
-    // Convert Uint8Array to Buffer
     const pdfBuffer = Buffer.from(pdfUint8Array);
 
     await browser.close();
