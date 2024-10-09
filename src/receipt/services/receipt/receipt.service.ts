609529c0ee07753767bc6ea2279bfac55fd5c585
sopheaktra_eang <sopheaktra.eang@wingbank.com.kh>
2024-10-09 14:21:57 +0700

    updated

--- src/receipt/services/receipt/receipt.service.ts
+++ src/receipt/services/receipt/receipt.service.ts
@@ -1,50 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { CreateReceiptDto } from '../../dtos/requests/create-receipt.dto';
 import * as hbs from 'handlebars';
+import * as fs from 'fs';
 import { join } from 'path';
-import { readFileSync } from 'fs';
-import { chromium } from '@playwright/test'; // Using Playwright
+import * as puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
 
 @Injectable()
 export class ReceiptService {
-  async create(
-    createReceiptDto: CreateReceiptDto,
-    baseUrl: string,
-  ): Promise<Buffer> {
-    const isDevelopment = process.env.NODE_ENV === 'production';
-    const templatePath = isDevelopment
-      ? join(
-          __dirname,
-          '..',
-          '..',
-          '..',
-          '..',
-          'dist',
-          'views',
-          'receipt-template.hbs',
-        )
-      : join(__dirname, '..', '..', '..', 'views', 'receipt-template.hbs');
-
-    // Read the Handlebars template from the file
-    const templateFile = readFileSync(templatePath, 'utf-8');
+  async create(createReceiptDto, baseUrl: string): Promise<Buffer> {
+    // Compile Handlebars template
+    const templatePath = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'views',
+      'receipt-template.hbs',
+    );
+    const templateFile = fs.readFileSync(templatePath, 'utf-8');
     const template = hbs.compile(templateFile);
+    const renderedHtml = template({ ...createReceiptDto, baseUrl });
 
-    // Render the HTML content using the HBS template
-    const htmlContent = template({ ...createReceiptDto, baseUrl });
-
-    // Set up Playwright with Chromium (without the need for any external browser)
-    const browser = await chromium.launch({
-      headless: true, // Always true in serverless environments
-      args: ['--no-sandbox', '--disable-setuid-sandbox'], // To avoid permission issues
+    // Use puppeteer-core with chrome-aws-lambda in production
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: true, // Run headless mode
+      defaultViewport: chromium.defaultViewport,
     });
+
     const page = await browser.newPage();
-    await page.setContent(htmlContent);
-    await page.addStyleTag({ url: `${baseUrl}/styles/tailwind.css` });
+    await page.setContent(renderedHtml, { waitUntil: 'networkidle0' });
 
-    // Generate the PDF from the rendered page
-    const pdfBuffer: Buffer = await page.pdf({ format: 'A6' });
+    const pdfBuffer = await page.pdf({
+      format: 'a6',
+      printBackground: true,
+    });
+
     await browser.close();
-
-    return pdfBuffer;
+    return Buffer.from(pdfBuffer);
   }
 }
